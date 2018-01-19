3fce26e81e9cc24632ff0bbb0b2006b42832ae47
Launcel <shay93@163.com>
2018-01-19 20:15:19 +0800

    add more datasoure

--- launcel-datasource/src/main/java/xyz/launcel/autoconfig/SessionFactoryAutoConfig.java
+++ launcel-datasource/src/main/java/xyz/launcel/autoconfig/SessionFactoryAutoConfig.java
@@ -7,14 +7,17 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import xyz.launcel.aspejct.ServerAspejct;
 import xyz.launcel.exception.ExceptionFactory;
 import xyz.launcel.interceptor.PageInterceptor;
+import xyz.launcel.lang.Json;
 import xyz.launcel.prop.DataSourcePropertie;
 import xyz.launcel.prop.MybatisPropertie;
 
@@ -38,6 +41,10 @@ public class SessionFactoryAutoConfig {
     @Primary
     @Bean(name = "dataSource")
     public HikariDataSource dataSource() {
+        System.out.println("-------------------------");
+        System.out.println(Json.toJson(mybatisPropertie));
+        System.out.println(Json.toJson(dataSourcePropertie));
+        System.out.println("-------------------------");
         return new HikariDataSource(dataSourcePropertie.getHikariConfig());
     }
 
@@ -67,6 +74,12 @@ public class SessionFactoryAutoConfig {
         mapperScannerConfigurer.setSqlSessionFactory(sqlSessionFactoryBean);
         mapperScannerConfigurer.setBasePackage(mybatisPropertie.getMapperPackage());
         return mapperScannerConfigurer;
+    }
+
+    @ConditionalOnProperty(prefix = "aspejct.service", value = "enabled", havingValue = "true")
+    @Bean
+    ServerAspejct serverAspejct() {
+        return new ServerAspejct();
     }
 
     /**
