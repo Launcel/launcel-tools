1419ab5328810a750c15b8830f8355ba6ee6d6a9
Launcel <Shay93@163.com>
2018-01-24 15:18:31 +0800

    mybatis generator更改

--- launcel-tools/launcel-gen/src/main/java/xyz/launcel/generator/codegen/mybatis3/xmlmapper/elements/BaseSqlElementGenerator.java
+++ launcel-tools/launcel-gen/src/main/java/xyz/launcel/generator/codegen/mybatis3/xmlmapper/elements/BaseSqlElementGenerator.java
@@ -23,42 +23,32 @@ public class BaseSqlElementGenerator extends AbstractXmlElementGenerator {
         this.context.getCommentGenerator().addComment(answer);
         StringBuilder sb = new StringBuilder();
         boolean and = false;
-        Iterator<IntrospectedColumn> var5 = this.introspectedTable.getPrimaryKeyColumns().iterator();
+        boolean first = true;
+        Iterator<IntrospectedColumn> var5 = this.introspectedTable.getAllColumns().iterator();
 
         IntrospectedColumn introspectedColumn;
-        while (var5.hasNext()) {
-            introspectedColumn = (IntrospectedColumn) var5.next();
-            if (and) {
-                sb.append("  AND ");
-            } else {
-                sb.append(" WHERE ");
-                and = true;
-            }
 
+        while (var5.hasNext()) {
+            if (first) {
+                sb.append("WHERE");
+                sb.append(" 1=1");
+                first = false;
+                answer.addElement(new LTextElement(sb.toString()));
+            }
+            sb.setLength(0);
+            introspectedColumn = var5.next();
+            LXmlElement isNotNullElement = new LXmlElement("if");
+            sb.setLength(0);
+            sb.append("param.").append(introspectedColumn.getJavaProperty());
+            sb.append(" != null");
+            isNotNullElement.addAttribute(new Attribute("test", sb.toString()));
+            sb.setLength(0);
+            sb.append("AND ");
             sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
             sb.append(" = ");
-            sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn, "obj."));
-            answer.addElement(new LTextElement(sb.toString()));
-        }
-
-        if (and) {
-            var5 = ListUtilities.removeGeneratedAlwaysColumns(this.introspectedTable.getNonPrimaryKeyColumns()).iterator();
-
-            while (var5.hasNext()) {
-                introspectedColumn = (IntrospectedColumn) var5.next();
-                LXmlElement isNotNullElement = new LXmlElement("if");
-                sb.setLength(0);
-                sb.append("obj.").append(introspectedColumn.getJavaProperty());
-                sb.append(" != null");
-                isNotNullElement.addAttribute(new Attribute("test", sb.toString()));
-                sb.setLength(0);
-                sb.append("AND ");
-                sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
-                sb.append(" = ");
-                sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn, "obj."));
-                isNotNullElement.addElement(new LTextElement(sb.toString()));
-                answer.addElement(isNotNullElement);
-            }
+            sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn, "param."));
+            isNotNullElement.addElement(new LTextElement(sb.toString()));
+            answer.addElement(isNotNullElement);
         }
 
         if (this.context.getPlugins().sqlMapSelectByPrimaryKeyElementGenerated(answer, this.introspectedTable)) {
