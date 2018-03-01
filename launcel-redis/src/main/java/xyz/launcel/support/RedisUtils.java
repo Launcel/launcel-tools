8a08fc54033902f64254c1f29dbab4f8d6326e16
Launcel <launcel93@gmail.com>
2018-03-01 19:10:27 +0800

    use interface replace static class

--- launcel-redis/src/main/java/xyz/launcel/support/RedisUtils.java
+++ launcel-redis/src/main/java/xyz/launcel/support/RedisUtils.java
@@ -17,135 +17,145 @@ import java.util.concurrent.TimeUnit;
  * 控制并发数据时，建议用JedisCommands(直接适用Jedis),见setNX方法<b>(但只能处理String类型)</b>
  */
 public class RedisUtils {
-
-    private RedisUtils() {
-    }
-
+    
     @SuppressWarnings("unchecked")
     private static RedisTemplate<String, Object> template = (RedisTemplate<String, Object>) ApplicationContextHook.getBean("redisTemplate");
-
-    private static Long expireTime = ((RedisProperties) ApplicationContextHook.getBean(RedisProperties.class)).getExptime();
 
     public static RedisTemplate<String, Object> getTemplate() {
         return template;
     }
 
-    public static void remove(final String... key) {
-        if (key.length <= 0)
-            ExceptionFactory.create("_REDIS__ERROR_CODE_001", "redis key is null");
-        for (String str : key)
-            remove(str);
+    private static final RedisInstance instance = new RedisInstance();
+
+    public static RedisInstance getInstance() {
+        return instance;
     }
 
-    public static void remove(final String key) {
-        if (exits(key))
-            template.delete(key);
-        else
-            ExceptionFactory.create("_REDIS__ERROR_CODE_004", "redis cannot find the value of this key");
+    private RedisUtils() {
     }
 
-    public static boolean exits(final String key) {
-        vidate(key);
-        return template.hasKey(key);
-    }
 
-    public static Object get(final String key) {
-        vidate(key);
-        return template.opsForValue().get(key);
-    }
+    private static class RedisInstance {
 
-    public static boolean set(final String key, final Object value) {
-        return set(key, value, expireTime);
-    }
+        private Long expireTime = ((RedisProperties) ApplicationContextHook.getBean(RedisProperties.class)).getExptime();
 
-    public static boolean set(final String key, final Object value, final Long expTime) {
-        return set(key, value, expTime, TimeUnit.SECONDS);
-    }
-
-    public static boolean set(final String key, final Object value, final Long expTime, TimeUnit timeUnit) {
-        try {
-            vidate(key, value, expTime);
-            template.opsForValue().set(key, value, expTime, timeUnit);
-            return true;
-        } catch (Exception x) {
-            return false;
+        public void remove(final String... key) {
+            if (key.length <= 0)
+                ExceptionFactory.create("_REDIS__ERROR_CODE_001", "redis key is null");
+            for (String str : key)
+                remove(str);
         }
 
-    }
+        public void remove(final String key) {
+            if (exits(key))
+                template.delete(key);
+            else
+                ExceptionFactory.create("_REDIS__ERROR_CODE_004", "redis cannot find the value of this key");
+        }
 
-    private static JedisCommands getCommands(RedisConnection connection) {
-        return (JedisCommands) connection.getNativeConnection();
-    }
+        public boolean exits(final String key) {
+            vidate(key);
+            return template.hasKey(key);
+        }
 
-    public static Boolean setNX(final String key, final String value, final Long expTime) {
-        vidate(key, value);
-        String str = template.execute((RedisCallback<String>) connection ->
-                getCommands(connection).set(key, value, "NX", "PX", expTime));
-        return StringUtils.isBlank(str);
-    }
+        public Object get(final String key) {
+            vidate(key);
+            return template.opsForValue().get(key);
+        }
 
-    /**
-     * RedisTemplate set方法默认使用的是jedis的setEX
-     *
-     * @param key
-     * @param value
-     * @return
-     */
-    public static String setEX(final String key, String value) {
-        return setEX(key, value, expireTime);
-    }
+        public boolean set(final String key, final Object value) {
+            return set(key, value, expireTime);
+        }
 
-    /**
-     * RedisTemplate set方法默认使用的是jedis的setEX
-     *
-     * @param key
-     * @param value
-     * @param expTime
-     * @return
-     */
-    public static String setEX(final String key, final String value, final Long expTime) {
-        vidate(key, value, expTime);
-        return template.execute((RedisCallback<String>) connection -> getCommands(connection).setex(key, expTime.intValue(), value));
-    }
+        public boolean set(final String key, final Object value, final Long expTime) {
+            return set(key, value, expTime, TimeUnit.SECONDS);
+        }
 
-    public static Object getAndSet(final String key, final Object value) {
-        vidate(value);
-        if (!exits(key))
-            ExceptionFactory.create("_REDIS__ERROR_CODE_004", "redis cannot find the value of this key");
+        public boolean set(final String key, final Object value, final Long expTime, TimeUnit timeUnit) {
+            try {
+                vidate(key, value, expTime);
+                template.opsForValue().set(key, value, expTime, timeUnit);
+                return true;
+            } catch (Exception x) {
+                return false;
+            }
+
+        }
+
+        private JedisCommands getCommands(RedisConnection connection) {
+            return (JedisCommands) connection.getNativeConnection();
+        }
+
+        public Boolean setNX(final String key, final String value, final Long expTime) {
+            vidate(key, value);
+            String str = template.execute((RedisCallback<String>) connection ->
+                    getCommands(connection).set(key, value, "NX", "PX", expTime));
+            return StringUtils.isBlank(str);
+        }
+
+        /**
+         * RedisTemplate set方法默认使用的是jedis的setEX
+         *
+         * @param key
+         * @param value
+         * @return
+         */
+        public String setEX(final String key, String value) {
+            return setEX(key, value, expireTime);
+        }
+
+        /**
+         * RedisTemplate set方法默认使用的是jedis的setEX
+         *
+         * @param key
+         * @param value
+         * @param expTime
+         * @return
+         */
+        public String setEX(final String key, final String value, final Long expTime) {
+            vidate(key, value, expTime);
+            return template.execute((RedisCallback<String>) connection -> getCommands(connection).setex(key, expTime.intValue(), value));
+        }
+
+        public Object getAndSet(final String key, final Object value) {
+            vidate(value);
+            if (!exits(key))
+                ExceptionFactory.create("_REDIS__ERROR_CODE_004", "redis cannot find the value of this key");
 //        return template.execute((RedisCallback<String>) connection ->
 //                Arrays.toString(connection.getSet(SafeEncoder.encode(key), SafeEncoder.encode(Json.toJson(value)))));
-        return template.opsForValue().getAndSet(key, value);
-    }
+            return template.opsForValue().getAndSet(key, value);
+        }
 
-    public static Long getExpTime(final String key) {
-        vidate(key);
-        return template.getExpire(key);
-    }
+        public Long getExpTime(final String key) {
+            vidate(key);
+            return template.getExpire(key);
+        }
 
-    private static void vidate(final Object value) {
-        vidate("1", value);
-    }
+        private void vidate(final Object value) {
+            vidate("1", value);
+        }
 
-    private static void vidate(final String key) {
-        vidate(key, true);
-    }
+        private void vidate(final String key) {
+            vidate(key, true);
+        }
 
-    private static void vidate(final String key, final Object value) {
-        vidate(key, value, 1L);
-    }
+        private void vidate(final String key, final Object value) {
+            vidate(key, value, 1L);
+        }
 
 
-    private static void vidate(final String key, final Long expTime) {
-        vidate(key, true, 1L);
-    }
+        private void vidate(final String key, final Long expTime) {
+            vidate(key, true, 1L);
+        }
 
-    private static void vidate(final String key, final Object value, final Long expTime) {
-        if (StringUtils.isBlank(key))
-            ExceptionFactory.create("_REDIS__ERROR_CODE_001", "redis key is null");
-        if (Objects.isNull(value))
-            ExceptionFactory.create("_REDIS__ERROR_CODE_002", "redis value is null");
-        if (expTime == null || expTime <= 0)
-            ExceptionFactory.create("_REDIS__ERROR_CODE_003", "redis expTime is error");
+        private void vidate(final String key, final Object value, final Long expTime) {
+            if (StringUtils.isBlank(key))
+                ExceptionFactory.create("_REDIS__ERROR_CODE_001", "redis key is null");
+            if (Objects.isNull(value))
+                ExceptionFactory.create("_REDIS__ERROR_CODE_002", "redis value is null");
+            if (expTime == null || expTime <= 0)
+                ExceptionFactory.create("_REDIS__ERROR_CODE_003", "redis expTime is error");
+        }
     }
 
 
