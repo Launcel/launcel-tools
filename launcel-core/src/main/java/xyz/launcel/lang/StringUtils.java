7d01889a961f6cfd9beee4d273de8ebb529b860c
Launcel <launcel93@gmail.com>
2018-04-19 00:16:53 +0800

    core更新

--- launcel-core/src/main/java/xyz/launcel/lang/StringUtils.java
+++ launcel-core/src/main/java/xyz/launcel/lang/StringUtils.java
@@ -1,34 +1,35 @@
 package xyz.launcel.lang;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Stream;
 
-public interface StringUtils {
+public class StringUtils {
     
     
-    static int indexOf(final CharSequence cs, final CharSequence searchChar, final int start) {
+    public static int indexOf(final CharSequence cs, final CharSequence searchChar, final int start) {
         return cs.toString().indexOf(searchChar.toString(), start);
     }
     
     
-    static boolean contains(final CharSequence seq, final CharSequence searchSeq) {
+    public static boolean contains(final CharSequence seq, final CharSequence searchSeq) {
         return seq != null && searchSeq != null && indexOf(seq, searchSeq, 0) >= 0;
     }
     
-    static boolean isNotEmpty(final CharSequence cs) {
+    public static boolean isNotEmpty(final CharSequence cs) {
         return !StringUtils.isEmpty(cs);
     }
     
-    static boolean isEmpty(final CharSequence cs) {
+    public static boolean isEmpty(final CharSequence cs) {
         return cs == null || cs.length() == 0;
     }
     
-    static boolean isNotBlank(final CharSequence cs) {
+    public static boolean isNotBlank(final CharSequence cs) {
         return !isBlank(cs);
     }
     
-    static boolean isBlank(final CharSequence cs) {
+    public static boolean isBlank(final CharSequence cs) {
         int strLen;
         if (cs == null || (strLen = cs.length()) == 0) {
             return true;
@@ -41,7 +42,7 @@ public interface StringUtils {
         return true;
     }
     
-    static String capitalize(final String str) {
+    public static String capitalize(final String str) {
         if (isBlank(str)) {
             return null;
         }
@@ -54,26 +55,105 @@ public interface StringUtils {
         return Character.toTitleCase(firstChar) + str.substring(1);
     }
     
-    static boolean isTrue(String s) {
+    public static boolean isTrue(String s) {
         return "true".equalsIgnoreCase(s);
     }
     
     
-    static boolean isFalse(String s) {
+    public static boolean isFalse(String s) {
         return "false".equalsIgnoreCase(s);
     }
     
-    static String getUUID() {
+    public static String getUUID() {
         return UUID.randomUUID().toString().replaceAll("-", "");
     }
     
     @SuppressWarnings("rawtypes")
-    static Stream spiltStream(String strings, String split) {
+    public static Stream spiltStream(String strings, String split) {
         return Arrays.stream(strings.split(split)).filter(StringUtils::isNotBlank);
     }
     
-    static void main(String[] args) {
-        System.out.println(capitalize("a"));
+    private static final Random RANDOM = new Random();
+    
+    public static String random(final int count) {
+        return random(count, false, true);
+    }
+    
+    private static String random(final int count, final boolean letters, final boolean numbers) {
+        return random(count, 0, 0, letters, numbers);
+    }
+    
+    private static String random(final int count, final int start, final int end, final boolean letters, final boolean numbers) {
+        return random(count, start, end, letters, numbers, null, RANDOM);
+    }
+    
+    private static String random(int count, int start, int end, final boolean letters, final boolean numbers,
+                                 final char[] chars, final Random random) {
+        if (count == 0) {
+            return "";
+        } else if (count < 0) {
+            throw new IllegalArgumentException("Requested random string length " + count + " is less than 0.");
+        }
+        if (chars != null && chars.length == 0) {
+            throw new IllegalArgumentException("The chars array must not be empty");
+        }
+        
+        if (start == 0 && end == 0) {
+            if (chars != null) {
+                end = chars.length;
+            } else {
+                if (!letters && !numbers) {
+                    end = Integer.MAX_VALUE;
+                } else {
+                    end = 'z' + 1;
+                    start = ' ';
+                }
+            }
+        } else {
+            if (end <= start) {
+                throw new IllegalArgumentException("Parameter end (" + end + ") must be greater than start (" + start + ")");
+            }
+        }
+        
+        final char[] buffer = new char[count];
+        final int    gap    = end - start;
+        
+        while (count-- != 0) {
+            char ch;
+            if (chars == null) {
+                ch = (char) (random.nextInt(gap) + start);
+            } else {
+                ch = chars[random.nextInt(gap) + start];
+            }
+            if (letters && Character.isLetter(ch)
+                    || numbers && Character.isDigit(ch)
+                    || !letters && !numbers) {
+                if (ch >= 56320 && ch <= 57343) {
+                    if (count == 0) {
+                        count++;
+                    } else {
+                        buffer[count] = ch;
+                        count--;
+                        buffer[count] = (char) (55296 + random.nextInt(128));
+                    }
+                } else if (ch >= 55296 && ch <= 56191) {
+                    if (count == 0) {
+                        count++;
+                    } else {
+                        buffer[count] = (char) (56320 + random.nextInt(128));
+                        count--;
+                        buffer[count] = ch;
+                    }
+                } else if (ch >= 56192 && ch <= 56319) {
+                    count++;
+                } else {
+                    buffer[count] = ch;
+                }
+            } else {
+                count++;
+            }
+        }
+        return new String(buffer);
     }
     
 }
