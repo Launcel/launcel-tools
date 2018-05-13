26c188a3d2822a1bc797df666a3f9accf71ce565
Launcel <Launcel93@gmail.com>
2018-05-13 22:40:14 +0800

    依赖更新

--- launcel-core/src/main/java/xyz/launcel/aspejct/ValidateAspejct.java
+++ launcel-core/src/main/java/xyz/launcel/aspejct/ValidateAspejct.java
@@ -13,19 +13,20 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 
 class ValidateAspejct extends BaseLogger {
-    
+
     void preparedArgs(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method          method    = signature.getMethod();
-        if (method.getName().toLowerCase().contains("upload")) {
-            DEBUG("调用了：{}.upload* 方法,可能是上传文件,不输出参数,如有误,请重命名该方法,不要包含upload关键字", signature.getDeclaringTypeName());
-            return;
-        }
+        Method method = signature.getMethod();
         if (isDebug()) {
-            DEBUG("调用了：{}.{} 方法 ：参数 \n{}", signature.getDeclaringTypeName(), method.getName(), Json.toJson(joinPoint.getArgs()));
+            if (method.getName().toLowerCase().contains("upload")) {
+                DEBUG("调用了：{}.upload* 方法,可能是上传文件,不输出参数,如有误,请重命名该方法,不要包含upload关键字", signature.getDeclaringTypeName());
+                return;
+            } else {
+                DEBUG("调用了：{}.{} 方法 ：参数 \n{}", signature.getDeclaringTypeName(), method.getName(), Json.toJson(joinPoint.getArgs()));
+            }
         }
         Parameter[] params = method.getParameters();
-        String      group  = StringUtils.capitalize(joinPoint.getSignature().getName());
+        String group = StringUtils.capitalize(joinPoint.getSignature().getName());
         for (int i = 0; i < params.length; i++) {
             if (params[i].isAnnotationPresent(Validate.class)) {
                 try {
@@ -37,10 +38,10 @@ class ValidateAspejct extends BaseLogger {
             }
         }
     }
-    
+
     void doReturn(JoinPoint joinPoint, Object object) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method          method    = signature.getMethod();
+        Method method = signature.getMethod();
         if (isDebug()) {
             DEBUG("调用了：{}.{} 方法结束 ：结果 \n{}", signature.getDeclaringTypeName(), method.getName(), Json.toJson(object));
         }
