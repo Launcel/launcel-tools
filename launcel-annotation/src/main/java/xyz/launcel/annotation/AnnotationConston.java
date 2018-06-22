b8d6fd9858b6539b3293d1c7d0bb8373d7a8513d
launcel <Launcel93@gmail.com>
2018-06-22 21:03:55 +0800

    1.1版本,更新

--- launcel-annotation/src/main/java/xyz/launcel/annotation/AnnotationConston.java
+++ launcel-annotation/src/main/java/xyz/launcel/annotation/AnnotationConston.java
@@ -4,7 +4,8 @@ public interface AnnotationConston
 {
     String point = "@annotation(xyz.launcel.annotation.ValidateMethod)";
 
-    String mapping = "@annotation(org.springframework.web.bind.annotation.GetMapping)" +
+    String mapping = "@annotation(xyz.launcel.annotation.ValidateMethod)" +
+            "|| @annotation(org.springframework.web.bind.annotation.GetMapping)" +
             "|| @annotation(org.springframework.web.bind.annotation.PostMapping) " +
             "|| @annotation(org.springframework.web.bind.annotation.PutMapping) " +
             "|| @annotation(org.springframework.web.bind.annotation.DeleteMapping)";
