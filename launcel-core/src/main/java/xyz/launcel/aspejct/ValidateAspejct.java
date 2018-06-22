327265f0a9eb439d31e9731cf54bd22f1850b47e
launcel <Launcel93@gmail.com>
2018-06-22 21:02:53 +0800

    1.1版本,更新

--- launcel-core/src/main/java/xyz/launcel/aspejct/ValidateAspejct.java
+++ launcel-core/src/main/java/xyz/launcel/aspejct/ValidateAspejct.java
@@ -8,11 +8,12 @@ import xyz.launcel.json.Json;
 import xyz.launcel.lang.StringUtils;
 import xyz.launcel.lang.ValidateUtils;
 import xyz.launcel.log.BaseLogger;
+import xyz.launcel.log.RootLogger;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 
-class ValidateAspejct extends BaseLogger {
+class ValidateAspejct {
     
     void preparedArgs(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
@@ -20,8 +21,8 @@ class ValidateAspejct extends BaseLogger {
         if (method.getName().toLowerCase().contains("upload")) {
             return;
         }
-        if (isDebug()) {
-            debug("调用了：" + signature.getDeclaringTypeName() + "." + method.getName() + " 方法 ：参数 \n" + Json.toJson(joinPoint.getArgs()));
+        if (RootLogger.isDebug()) {
+            RootLogger.debug("调用了：" + signature.getDeclaringTypeName() + "." + method.getName() + " 方法 ：参数 \n" + Json.toJson(joinPoint.getArgs()));
         }
         Parameter[] params = method.getParameters();
         String      group  = StringUtils.capitalize(joinPoint.getSignature().getName());
@@ -40,8 +41,8 @@ class ValidateAspejct extends BaseLogger {
     void doReturn(JoinPoint joinPoint, Object object) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method          method    = signature.getMethod();
-        if (isDebug()) {
-            debug("调用：" + signature.getDeclaringTypeName() + "." + method.getName() + " 方法结束 ：结果 \n" + Json.toJson(object));
+        if (RootLogger.isDebug()) {
+            RootLogger.debug("调用：" + signature.getDeclaringTypeName() + "." + method.getName() + " 方法结束 ：结果 \n" + Json.toJson(object));
         }
     }
 }
