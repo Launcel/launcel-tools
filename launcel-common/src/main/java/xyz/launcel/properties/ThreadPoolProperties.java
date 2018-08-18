c015292d709536d7aed137e2154780b69a81b431
launcel <Launcel93@gmail.com>
2018-08-18 16:11:09 +0800

    common task添加

---
+++ launcel-common/src/main/java/xyz/launcel/properties/ThreadPoolProperties.java
@@ -0,0 +1,44 @@
+package xyz.launcel.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by launcel on 2018/8/6.
+ */
+@ConfigurationProperties(prefix = "common.executor")
+public class ThreadPoolProperties
+{
+    private Integer corePoolSize  = 1;
+    private Integer maxPoolSize   = 8;
+    private Integer queueCapacity = 10;
+
+    public Integer getCorePoolSize()
+    {
+        return corePoolSize;
+    }
+
+    public void setCorePoolSize(Integer corePoolSize)
+    {
+        this.corePoolSize = corePoolSize;
+    }
+
+    public Integer getMaxPoolSize()
+    {
+        return maxPoolSize;
+    }
+
+    public void setMaxPoolSize(Integer maxPoolSize)
+    {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    public Integer getQueueCapacity()
+    {
+        return queueCapacity;
+    }
+
+    public void setQueueCapacity(Integer queueCapacity)
+    {
+        this.queueCapacity = queueCapacity;
+    }
+}
