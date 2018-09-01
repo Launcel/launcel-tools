2f7d16602b1eaf93dee6757e6d06be0a0ca386db
launcel <Launcel93@gmail.com>
2018-09-01 14:51:45 +0800

    自动日志切换

--- launcel-boot-start/src/main/java/xyz/launcel/listener/LoggerEnvironListener.java
+++
@@ -1,9 +0,0 @@
-package xyz.launcel.listener;
-
-import org.springframework.boot.context.logging.LoggingApplicationListener;
-
-/**
- * Created by launcel on 2018/8/31.
- */
-public class LoggerEnvironListener extends LoggingApplicationListener
-{}
