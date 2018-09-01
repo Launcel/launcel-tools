303596f2c106eccc2ac40f49441c1e35dfd46f9b
launcel <Launcel93@gmail.com>
2018-09-01 11:57:08 +0800

    自定义扩展yml

--- launcel-boot-start/src/main/java/xyz/launcel/listener/ApplicatonEnvironListener.java
+++ launcel-boot-start/src/main/java/xyz/launcel/listener/ApplicatonEnvironListener.java
@@ -57,6 +57,6 @@ public class ApplicatonEnvironListener implements SpringApplicationRunListener, 
     @Override
     public int getOrder()
     {
-        return 1;
+        return LOWEST_PRECEDENCE;
     }
 }
