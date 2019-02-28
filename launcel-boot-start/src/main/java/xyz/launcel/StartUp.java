dd16c5d9fdd09a2c9768e047ea37ac30d9dad11b
launcel <Launcel93@gmail.com>
2019-02-28 15:11:10 +0800

    logback修改

--- launcel-boot-start/src/main/java/xyz/launcel/StartUp.java
+++ launcel-boot-start/src/main/java/xyz/launcel/StartUp.java
@@ -19,12 +19,14 @@ public interface StartUp
         var cac  = new SpringApplicationBuilder(appClass).web(WebApplicationType.SERVLET).bannerMode(Banner.Mode.OFF).run(args);
         var env  = cac.getEnvironment();
         var port = StringUtils.isEmpty(env.getProperty("server.port")) ? "8080" : env.getProperty("server.port");
-        RootLogger.error("app port is : \t{}, \n\t\turl is : \thttp://localhost:", port, port);
+
+        String log = "app port is : \t{}, \n\t\turl   is : \thttp://localhost:{}";
         try
         {
             var addr = InetAddress.getLocalHost();
             var ip   = addr.getHostAddress();
-            RootLogger.error("app port is : \t{}, \n\t\turl is : \thttp://{}", port, ip + port);
+            log += "\n\t\treal  url  is : \thttp://{}";
+            RootLogger.error(log, port, port, ip + ":" + port);
         }
         catch (UnknownHostException e)
         {
