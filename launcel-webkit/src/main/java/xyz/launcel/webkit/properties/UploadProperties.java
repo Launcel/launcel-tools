b86fd458f85fb83b02eb1e85d4a641f3e5288e89
launcel <Launcel93@gmail.com>
2020-04-18 23:46:53 +0800

    修改工具

--- launcel-webkit/src/main/java/xyz/launcel/webkit/properties/UploadProperties.java
+++ launcel-webkit/src/main/java/xyz/launcel/webkit/properties/UploadProperties.java
@@ -1,6 +1,7 @@
 package xyz.launcel.webkit.properties;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import xyz.launcel.common.utils.CollectionUtils;
 import xyz.launcel.common.utils.StringUtils;
@@ -10,7 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-//@Setter
+@Setter
 @Getter
 @ConfigurationProperties(prefix = "web.upload")
 public class UploadProperties
@@ -33,37 +34,43 @@ public class UploadProperties
 
     public void setDomain(String domain)
     {
-        UploadConfig.domain = this.domain = StringUtils.isBlank(domain) ? "" : domain;
+        this.domain = StringUtils.isBlank(domain) ? "" : domain;
+        UploadConfig.domain = this.domain;
     }
 
     public void setMaxSize(Long maxSize)
     {
-        UploadConfig.maxSize = this.maxSize = Objects.isNull(maxSize) ? Long.MAX_VALUE : maxSize;
+        this.maxSize = Objects.isNull(maxSize) ? Long.MAX_VALUE : maxSize;
+        UploadConfig.maxSize = this.maxSize;
     }
 
     public void setMinSize(Long minSize)
     {
-        UploadConfig.minSize = this.minSize = Objects.isNull(minSize) ? 1024L : minSize;
+        this.minSize = Objects.isNull(minSize) ? 1024L : minSize;
+        UploadConfig.minSize = this.minSize;
     }
 
     public void setPath(String path)
     {
-        UploadConfig.path = this.path = StringUtils.isNotBlank(path) ? path : File.separator.concat("Users")
+        this.path = StringUtils.isNotBlank(path) ? path : File.separator.concat("Users")
                 .concat(File.separator)
                 .concat("tmp")
                 .concat(File.separator)
                 .concat("upload");
+        UploadConfig.path = this.path;
     }
 
     public void setFileType(List<String> fileType)
     {
-        UploadConfig.fileType = this.fileType = CollectionUtils.isEmpty(fileType) ? Arrays.asList("doc", "docx", "pdf", "jpg", "png", "jepg") : fileType;
+        this.fileType = CollectionUtils.isEmpty(fileType) ? Arrays.asList("doc", "docx", "pdf", "jpg", "png", "jepg") : fileType;
+        UploadConfig.fileType = this.fileType;
     }
 
     public void setContentType(List<String> contentType)
     {
-        UploadConfig.contentType = this.contentType = CollectionUtils.isEmpty(contentType) ? Arrays.asList("504b0304", "d0cf11e0", "25504446", "ffd8ffe0",
-                "ffd8ffe1", "89504e47", "ffd8ff") : contentType;
+        this.contentType = CollectionUtils.isEmpty(contentType) ? Arrays.asList("504b0304", "d0cf11e0", "25504446", "ffd8ffe0", "ffd8ffe1", "89504e47",
+                "ffd8ff") : contentType;
+        UploadConfig.contentType = this.contentType;
     }
 
     public static UploadConfig getConfig()
